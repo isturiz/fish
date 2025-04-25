5d76c96555ae69b25376f47f1b1c9482c10c7cb0
Mauricio Istúriz <isturizmauricio@gmail.com>
2025-04-25 19:49:10 -0400

    update config: idr why this

--- config.fish
+++ config.fish
@@ -1,24 +1,28 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    # set PATH /usr/local/bin $PATH
-    set -gx PATH /opt/homebrew/bin $PATH
-    # if command -q zellij
-    #     eval (zellij setup --generate-auto-start fish | string collect)
-    # else
-    #     echo "zellij no está disponible en el PATH."
-    # end
+    # Add /opt/homebrew/bin to PATH if it exists
+    if test -d /opt/homebrew/bin
+        set -gx PATH /opt/homebrew/bin $PATH
+    end
+
+    # Add pipx bin to PATH if it exists
+    if test -d ~/.local/bin
+        set -gx PATH $PATH ~/.local/bin
+    end
 end
 
-# Carga Homebrew environment
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# Load Homebrew environment if installed
+if test -x /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
+end
 
-# Carga nvm.fish
+# Load nvm.fish if nvm is installed
 if type -q nvm
     nvm use latest > /dev/null 2>&1
 end
 
-# Establece variables de entorno
+# Set environment variables
 set -x HOMEBREW_NO_ENV_HINTS 1
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
 set -gx PATH $HOME/.local/bin $PATH
+
